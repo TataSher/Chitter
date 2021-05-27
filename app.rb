de9324e07bcc9f6f656299d43050caa8e7565c7c
Natalia_Sherchenkova <tataterer@gmail.com>
2021-05-27 13:59:44 +0100

    Set up file with Capybara, Sinatra and RSpec

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Chitter < Sinatra::base
+  configure :development do 
+    register Sinatra::reloader
+  end
+
+  get '/' do
+    'Hello World'
+  end
+
+  run! if app_file == $0
+end
+
