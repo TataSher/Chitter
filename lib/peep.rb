16132773def6de53c19d1408ef1cbc6a902878c9
Natalia_Sherchenkova <tataterer@gmail.com>
2021-06-02 13:57:22 +0100

    Database chitter_db_development connected into Peep model

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,9 +1,10 @@
+require 'pg'
 # class Peep allows to create peep instances
-
 class Peep
 
   def self.list
-    ['First Peep', 'Second Peep', 'Third Peep']
+    connection = PG.connect(dbname: 'chitter_db_development')
+    result = connection.exec( "SELECT * FROM peeps" )
+    result.map { |message| message['peep']}
   end
-
 end
