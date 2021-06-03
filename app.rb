d9796ad9ef07301923a570b53e56ee82f099b0a2
Natalia_Sherchenkova <tataterer@gmail.com>
2021-06-03 19:31:04 +0100

    posting peeps feature created. Need to add connection to parameters

--- app.rb
+++ app.rb
@@ -19,5 +19,17 @@ class Chitter < Sinatra::Base
     erb :'peeps/index'
   end
 
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps' do
+    p params
+    connection = PG.connect(dbname: 'chitter_db_development')
+    connection.exec("INSERT INTO peeps (peep, written_by) VALUES ('First Peep' , '1');")
+    redirect '/peeps'
+  end
+
+
   run! if app_file == $PROGRAM_NAME
 end
--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -1,10 +1,11 @@
-# feature 'Posting Peeps' do
-#   scenario 'A user can post a peep' do
-#     visit('/peeps/new')
-#     fill_in 'enter_peep' , with: 'First Peep'
-#     click_on('Peep')
+feature 'Posting Peeps' do
+  scenario 'A user can post a peep' do
+    visit('/peeps')
+    click_on 'Peep'
+    fill_in 'enter_peep' , with: 'First Peep'
+    click_on 'Peep'
 
-#     expect(page).to have_content('First Peep')
+    expect(page).to have_content('First Peep')
     
-#   end
-# end
+  end
+end
